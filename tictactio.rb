1d3a2ab5accf88ebf79d80cd03e5f60e37b9d379
Laura Rossi <laura.rossi@wework.com>
2018-02-16 00:46:00 +0000

    automatically backed up by learn

---
+++ tictactio.rb
@@ -0,0 +1,11 @@
+ 
+ 
+ 
+ expect(WIN_COMBINATIONS).to include_array([0,1,2])
+      expect(WIN_COMBINATIONS).to include_array([3,4,5])
+      expect(WIN_COMBINATIONS).to include_array([6,7,8])
+      expect(WIN_COMBINATIONS).to include_array([0,3,6])
+      expect(WIN_COMBINATIONS).to include_array([1,4,7])
+      expect(WIN_COMBINATIONS).to include_array([2,5,8])
+      expect(WIN_COMBINATIONS).to include_array([0,4,8])
+      expect(WIN_COMBINATIONS).to include_array([6,4,2])
